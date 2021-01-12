ea8c2d6a0f5e1582a2c01655c716f93e5801e296
Richard Ohana <ohanar@wwu.edu>
2021-01-12 11:33:00 -0800

    Completed Contact page. Could possibly use work on resizing

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -3,12 +3,31 @@ import './Contact.css';
 
 import Navbar from '../Navbar/Navbar';
 
+import ContactInfo from './ContactInfo/ContactInfo';
+
 class Contact extends React.Component {
     render() {
         return (
             <div class="Contact">
                 <div className="contact-background-image">
                     <Navbar />
+                    <div className="container">
+                        <span>Contact Us</span>
+                        <hr></hr>
+                        <h2>We'd {"\u2661"} to help!</h2>
+                        <p>We want to serve you as best as possible, so let us know if you have any questions!</p>
+                        <div className="contact-info-container">
+                            <form>
+                                <input type="name" id="name" name="name" placeholder="Name"></input>
+                                <input placeholder="Email"></input>
+                                <textarea placeholder="Message"></textarea>
+                                <input type="submit"></input>
+                            </form>
+                            <ContactInfo />
+                            
+                        </div>
+                    
+                    </div>
                 </div>
             </div>
         )
