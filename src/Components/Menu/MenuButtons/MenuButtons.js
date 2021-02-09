42fac709b1b1c6fa7fc0613f2d262fa97b2eb2bd
Richard Ohana <ohanar@wwu.edu>
2021-02-08 19:13:50 -0800

    Add dimming properties to MenuButtons when user clicks Smoothie component. Needs work on Acai Bowls button

--- src/Components/Menu/MenuButtons/MenuButtons.js
+++ src/Components/Menu/MenuButtons/MenuButtons.js
@@ -5,12 +5,12 @@ class MenuButtons extends React.Component {
     render() {
         return (
             <div className="MenuButtons">
-                <button onClick={() => this.props.hideComponent("showComplete")}>Complete Menu</button>
-                <button onClick={() => this.props.hideComponent("showGreen")}>Go Green</button>
-                <button onClick={() => this.props.hideComponent("showBulk")}>Bulk Me Up</button>
-                <button onClick={() => this.props.hideComponent("showNourish")}>Nourish My Body</button>                
-                <button onClick={() => this.props.hideComponent("showImmunity")}>Boost My Immune System</button>
-                <button onClick={() => this.props.hideComponent("showWeight")}>Help Me Lose Weight</button>
+                <button className={this.props.dim} onClick={() => this.props.hideComponent("showComplete")}>Complete Menu</button>
+                <button className={this.props.dim} onClick={() => this.props.hideComponent("showGreen")}>Go Green</button>
+                <button className={this.props.dim} onClick={() => this.props.hideComponent("showBulk")}>Bulk Me Up</button>
+                <button className={this.props.dim} onClick={() => this.props.hideComponent("showNourish")}>Nourish My Body</button>                
+                <button className={this.props.dim} onClick={() => this.props.hideComponent("showImmunity")}>Boost My Immune System</button>
+                <button className={this.props.dim} onClick={() => this.props.hideComponent("showWeight")}>Help Me Lose Weight</button>
                 <button>Acai Bowls</button>
             </div>
         )
