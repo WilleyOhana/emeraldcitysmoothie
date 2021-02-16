7709d6f5de4dcd13691282e7a56a784e3ef3af59
Richard Ohana <ohanar@wwu.edu>
2021-02-16 15:23:36 -0800

    Add alert to notify contact form does not work yet

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -6,6 +6,15 @@ import Navbar from '../Navbar/Navbar';
 import ContactInfo from './ContactInfo/ContactInfo';
 
 class Contact extends React.Component {
+
+    submit = () => {
+        let form = document.getElementById('form');
+
+        alert("Still building this functionality. Please try again on a later date. If you need to get in touch, please email us at ecs.bellingham@gmail.com");
+
+        form.innerHTML = "<h2>Thank you for your submission. We will get back to you as soon as we can!</h2>";
+    }
+
     render() {
         return (
             <div class="Contact">
@@ -17,7 +26,7 @@ class Contact extends React.Component {
                         <h2>We'd {"\u2661"} to help!</h2>
                         <p>We want to serve you as best as possible, so let us know if you have any questions!</p>
                         <div className="contact-info-container">
-                            <form>
+                            <form id="form" onSubmit={this.submit}>
                                 <input type="name" id="name" name="name" placeholder="Name"></input>
                                 <input placeholder="Email"></input>
                                 <textarea placeholder="Message"></textarea>
