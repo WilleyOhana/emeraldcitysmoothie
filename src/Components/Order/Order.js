a3d56a94d49243ed5ed2b1e8c8b92dedd81df461
Richard Ohana <ohanar@wwu.edu>
2021-03-19 20:11:05 -0700

    Temporary order page build

--- src/Components/Order/Order.js
+++ src/Components/Order/Order.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import './Order.css';
+import Navbar from '../Navbar/Navbar';
+
+import vikingfoodspic from './vikingfoods.png';
+import doordashpic from './doordash.jpg';
 
 class Order extends React.Component {
     render() {
         return (
-            <div class="Order">"Hello!"</div>
+            <div className="Order">
+                <Navbar />
+
+                <div className="temp-contents">
+                    <span>Online ordering coming soon!</span>
+                    <br></br>
+                    <br></br>
+                    <span>For now, feel free to order through our third party delivery services:</span>
+                    <br></br>
+                    <br></br>
+                    <a href="https://www.ordervikingfood.com/r/1580/restaurants/delivery/Smoothies-Cafe/Emerald-City-Smoothie-Bellingham">
+                        <img src={vikingfoodspic} alt="Viking Foods"></img>
+                    </a>
+                    <a href="https://www.doordash.com/store/emerald-city-smoothie-bellingham-970293/">
+                        <img src={doordashpic} alt="Viking Foods"></img>
+                    </a>
+                </div>
+            </div>
         )
     }
 }
