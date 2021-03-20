8febf02a7f9bdc60597d91f83ed9923e43c1c566
Richard Ohana <ohanar@wwu.edu>
2021-03-19 19:17:51 -0700

    Add responsive design to Navbar component. Could still use some styling help

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,21 +1,54 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import './Navbar.css';
+
+import hamburgericon from './hamburger.png';
 import logo from './ECSCircleLogo.jpg';
 
 class Navbar extends React.Component {
+
+    displayDropdown() {
+        let Navbar = document.getElementById('Navbar');
+
+        if(Navbar.classList.contains('responsive')) {
+            Navbar.classList.remove('responsive');
+        } else {
+            Navbar.classList.add('responsive');
+        }
+
+        let dropdownContents = document.getElementById('responsive-contents');
+        
+        if(dropdownContents.classList.contains('off')) {
+            dropdownContents.classList.remove('off');
+            dropdownContents.classList.add('on');
+        } else {
+            dropdownContents.classList.remove('on');
+            dropdownContents.classList.add('off');
+        }
+    }
+
     render() {
         return (
             <div className="Navbar" id="Navbar">
                 <Link to="/">
                     <img className="logo" src={logo} alt="Emerald City Smoothie Logo"></img>
                 </Link>
-                <div className="contents" id="contents">
+                <div className="contents">
                     <NavLink to="/" exact className="button">Home</NavLink>
                     <NavLink to="/menu" className="button">Menu</NavLink>
                     <NavLink to="/contact" className="button">Contact</NavLink>
                     <NavLink to="/about" className="button">About</NavLink>
+                    <img id="dropdown" className="hamburger" src={hamburgericon} alt="menu" onClick={this.displayDropdown}></img>
                     <NavLink to="/order" className="button order">Order Now</NavLink>
+                </div>
+                <div id="responsive-contents" className="responsive-contents off">
+                    <NavLink to="/" exact className="button">Home</NavLink>
+                    <hr></hr>
+                    <NavLink to="/menu" className="button">Menu</NavLink>
+                    <hr></hr>
+                    <NavLink to="/contact" className="button">Contact</NavLink>
+                    <hr></hr>
+                    <NavLink to="/about" className="button">About</NavLink>
                 </div>
             </div>
         );
