ce0e471fe0f92d272a3395294dab226aa3ed7717
Richard Ohana <ohanar@wwu.edu>
2021-05-01 14:59:05 -0700

    Add form processing and validation & email functionality

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -4,15 +4,63 @@ import './Contact.css';
 import Navbar from '../Navbar/Navbar';
 
 import ContactInfo from './ContactInfo/ContactInfo';
+import emailjs from 'emailjs-com';
 
 class Contact extends React.Component {
+    constructor(props) {
+        super(props);
 
-    submit = () => {
-        let form = document.getElementById('form');
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-        alert("Still building this functionality. Please try again on a later date. If you need to get in touch, please email us at ecs.bellingham@gmail.com");
+    validateForm = (e) => {
 
-        form.innerHTML = "<h2>Thank you for your submission. We will get back to you as soon as we can!</h2>";
+        const name = e.target[0].value;
+        const email = e.target[1].value;
+        const message = e.target[2].value;
+
+        const validateEmail = (emailTest) => {
+            const regEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            return regEx.test(emailTest);
+        }
+
+        if(name.length < 1 && name.length > 50) {
+            alert("Please enter a valid name.");
+            return false;
+        } else if (!validateEmail(email)) {
+            alert("Please enter a valid email address.")
+            return false;
+        } else if (message.length < 1) {
+            alert("Please enter a message.");
+            return false;
+        }
+
+        return true;
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(this.validateForm(e)) {
+
+            //Send automated message to customer
+            emailjs.sendForm('service_6bv5quk', 'template_62ubyqk', e.target, 'user_0hYTlz6Tj87gdtKnWoQot')
+                .then(() => {
+
+                    //Send automated message to store
+                    emailjs.sendForm('service_6bv5quk', 'template_c8cwbdb', e.target, 'user_0hYTlz6Tj87gdtKnWoQot')
+                        .then(() => {
+                            
+                            //Clear form data
+                            e.target[0].value = '';
+                            e.target[1].value = '';
+                            e.target[2].value = '';
+
+                            alert('Email sent!');
+                        })
+                });
+        } 
+        
     }
 
     render() {
@@ -26,11 +74,11 @@ class Contact extends React.Component {
                         <h2>We'd {"\u2661"} to help!</h2>
                         <p>We want to serve you as best as possible, so let us know if you have any questions!</p>
                         <div className="contact-info-container">
-                            <form id="form" onSubmit={this.submit}>
-                                <input type="name" id="name" name="name" placeholder="Name"></input>
-                                <input placeholder="Email"></input>
-                                <textarea placeholder="Message"></textarea>
-                                <input type="submit"></input>
+                            <form id="contact-form" onSubmit={this.handleSubmit}>
+                                <input name="to_name" placeholder="Name" ></input>
+                                <input name="to_email" placeholder="Email"></input>
+                                <textarea name="message" placeholder="Message"></textarea>
+                                <input className="submit" type="submit" value="Send"></input>
                             </form>
                             <ContactInfo />
                             
